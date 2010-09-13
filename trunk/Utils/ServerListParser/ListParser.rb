762c33b13f85cef2d6a51a9f9be0a234b2bbd534
zenden2k <zenden2k@f7539788-94a2-11de-9c1f-9d2f795fff43>
2010-09-13 08:46:04 +0000

    Added servers.xml parser written in Ruby

---
+++ trunk/Utils/ServerListParser/ListParser.rb
@@ -0,0 +1,51 @@
+=begin
+    Image Uploader - program for uploading images/files to Internet
+    Copyright (C) 2007-2010 ZendeN <zenden2k@gmail.com>
+	 
+    HomePage:    http://zenden.ws/imageuploader
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+#  This script scans servers.xml file and generates HTML code  
+#  containing list of server names preceded by their favicons
+#  and saves it into file servers_html.txt in current folder
+#
+#  Unfortunately this script doesn't parse servers.xml as XML file.  
+#  It simply scans it for server names using regular expression.
+
+def file_put_contents( name, *contents )
+  File.open( name, "w" ){ |file|
+    contents.each{ |item|
+      file << item
+    }
+  }
+end
+
+dataFolder =  '../../Data/'
+filename= 'servers.xml'
+lines = File.readlines(dataFolder  + filename)
+line_count = lines.size
+text = lines.join
+serverNames = text.scan(/Name=\"(.+?)\"/)
+
+resultText = '';
+serverNames.each do |nam|
+	servName = nam[0]
+	if( File::exists?( dataFolder + "Favicons/" + servName + ".ico" ) )
+		resultText<< "<img src='images/servericons/"+ servName.downcase + ".ico' align='middle' width='16' height='16'>&nbsp;&nbsp;&nbsp;&nbsp;";
+		end;
+		resultText << servName << "<br/>\n";
+end
+file_put_contents("servers_html.txt", resultText)
