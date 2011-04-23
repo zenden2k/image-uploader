406fb9790e29c0e3752a44edc708c16ec163484e
zenden2k@gmail.com <zenden2k@gmail.com@f7539788-94a2-11de-9c1f-9d2f795fff43>
2011-04-23 21:13:12 +0000

    Upload missing files

---
+++ Source/ServerListTool/ServerListTool.cpp
@@ -0,0 +1,45 @@
+// ServerListTool.cpp : main source file for ServerListTool.exe
+//
+
+
+
+#include "resource.h"
+
+#include "MainDlg.h"
+#include <Gdiplus.h>
+CAppModule _Module;
+#include "../Func/Common.h"
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR /*lpstrCmdLine*/, int /*nCmdShow*/)
+{
+	HRESULT hRes = ::CoInitialize(NULL);
+// If you are running on NT 4.0 or higher you can use the following call instead to 
+// make the EXE free threaded. This means that calls come in on a random RPC thread.
+//	HRESULT hRes = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	ATLASSERT(SUCCEEDED(hRes));
+
+	// this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
+	::DefWindowProc(NULL, 0, 0, 0L);
+
+	AtlInitCommonControls(ICC_BAR_CLASSES);	// add flags to support other controls
+
+	hRes = _Module.Init(NULL, hInstance);
+	ATLASSERT(SUCCEEDED(hRes));
+
+	GdiplusStartupInput gdiplusStartupInput;
+
+	ULONG_PTR gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	int nRet = 0;
+	// BLOCK: Run application
+	{
+		CMainDlg dlgMain;
+		nRet = dlgMain.DoModal();
+	}
+
+	_Module.Term();
+	SquirrelVM::Shutdown(); 
+	::CoUninitialize();
+	ClearTempFolder(IUTempFolder); 
+	return nRet;
+}
