fe62f49abd924dfd17022652871c79bf15d9e678
zenden2k <zenden2k@gmail.com>
2015-05-02 14:04:58 +0300

    Docs update

--- api/html/search/all_5.js
+++ api/html/search/all_5.js
@@ -2,8 +2,13 @@ var searchData=
 [
   ['getchildren',['getChildren',['../class_script_a_p_i_1_1_html_element.html#a184f268935e45cb5f125008c27690eca',1,'ScriptAPI::HtmlElement']]],
   ['getcount',['GetCount',['../class_c_folder_list.html#a9cbf98c17d6b7027e60c59fb8ad69a85',1,'CFolderList']]],
+  ['getdeleteurl',['getDeleteUrl',['../class_c_i_u_upload_params.html#a3be9179883d2494024bf696f0e4fb36e',1,'CIUUploadParams']]],
+  ['getdirecturl',['getDirectUrl',['../class_c_i_u_upload_params.html#ac7a295cb6264d815988eb10ea732ce19',1,'CIUUploadParams']]],
   ['getdocumentcontents',['getDocumentContents',['../class_script_a_p_i_1_1_c_web_browser.html#a1412e421db5a55c76ecd25ca324a1d8d',1,'ScriptAPI::CWebBrowser']]],
+  ['getediturl',['getEditUrl',['../class_c_i_u_upload_params.html#ae3df291ee1c8617e475fc3670591d6fe',1,'CIUUploadParams']]],
   ['getfolderaccesstypelist',['GetFolderAccessTypeList',['../implement_8h.html#ae33a0f10bddfe575bd4d42d893b43050',1,'implement.h']]],
   ['getserverparamlist',['GetServerParamList',['../implement_8h.html#a8876187be59761cde85c6da06d2dfd97',1,'implement.h']]],
-  ['getvalue',['getValue',['../class_script_a_p_i_1_1_html_element.html#a389c3f368bb4178148a634d47244a7f0',1,'ScriptAPI::HtmlElement']]]
+  ['getthumburl',['getThumbUrl',['../class_c_i_u_upload_params.html#a5948c0179e10ab4adc53b86dc65d0400',1,'CIUUploadParams']]],
+  ['getvalue',['getValue',['../class_script_a_p_i_1_1_html_element.html#a389c3f368bb4178148a634d47244a7f0',1,'ScriptAPI::HtmlElement']]],
+  ['getviewurl',['getViewUrl',['../class_c_i_u_upload_params.html#a6195516ecdd12877188098697110bf89',1,'CIUUploadParams']]]
 ];
--- api/html/search/classes_6.js
+++ api/html/search/classes_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['serversync',['ServerSync',['../class_server_sync.html',1,'']]],
+  ['serversyncexception',['ServerSyncException',['../class_server_sync_exception.html',1,'']]],
   ['simplexml',['SimpleXml',['../class_simple_xml.html',1,'']]],
   ['simplexmlnode',['SimpleXmlNode',['../class_simple_xml_node.html',1,'']]]
 ];
