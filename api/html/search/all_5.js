3d5f7dbf55a470ff2a38a2da299b3bddf1fcaa62
zenden2k <zenden2k@gmail.com>
2015-05-03 18:09:49 +0300

    Docs update

--- api/html/search/all_5.js
+++ api/html/search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['fileuploadtaskwrapper',['FileUploadTaskWrapper',['../class_script_a_p_i_1_1_file_upload_task_wrapper.html',1,'ScriptAPI']]],
-  ['functions_2ecpp',['Functions.cpp',['../_functions_8cpp.html',1,'']]]
+  ['functions_2eh',['Functions.h',['../_functions_8h.html',1,'']]]
 ];
--- api/html/search/files_0.js
+++ api/html/search/files_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['functions_2ecpp',['Functions.cpp',['../_functions_8cpp.html',1,'']]]
+  ['functions_2eh',['Functions.h',['../_functions_8h.html',1,'']]]
 ];
