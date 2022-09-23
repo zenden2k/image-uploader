1370d37f001a7fbfe6c71ed87c1cdb4a73d8b294
Sergey Svistunov <zenden2k@gmail.com>
2022-09-23 15:46:29 +0300

    Added a python script for uploading 'servers.xml' and site icons to svistunov.dev

---
+++ Dist/uploadlist.py
@@ -0,0 +1,19 @@
+import requests, sys, shutil
+
+REMOTE_URL = 'https://svistunov.dev/update-server-list'
+DATA_DIR = sys.path[0] + '/../Data/'
+
+if len(sys.argv) < 2:
+    print("You should password as argument.")
+    sys.exit(1)
+filename = sys.path[0] + '/favicons'
+shutil.make_archive(filename, 'zip', DATA_DIR + 'Favicons')
+
+files = {
+    'file': open(DATA_DIR + 'servers.xml','rb'),
+    'icons': open(filename + '.zip','rb'),
+}
+values = {'password': sys.argv[1]}
+
+r = requests.post(REMOTE_URL, files=files, data=values,  headers={'Accept': 'application/json'})
+print(r.text)
