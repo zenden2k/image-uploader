ff7b2e3dd5ead75ffb104014401c46a3442c4637
Sergey Svistunov <zenden2k@gmail.com>
2023-06-10 08:55:12 +0300

    Add resource files for CLI and qimageuploader

--- Source/qimageuploader/Gui/AboutDialog.cpp
+++ Source/qimageuploader/Gui/AboutDialog.cpp
@@ -1,13 +1,16 @@
 #include "AboutDialog.h"
 
 #include "ui_AboutDialog.h"
+#include "Core/AppParams.h"
 
 AboutDialog::AboutDialog(QWidget *parent): 
     QDialog(parent),
     ui(new Ui::AboutDialog) {
     ui->setupUi(this);
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
-    ui->versionLabel->setText("v0.1.2");
+    auto version = AppParams::instance()->GetAppVersion();
+
+    ui->versionLabel->setText(QString::fromStdString(version->FullVersion) + " build " + QString::number(version->Build));
 }
 
 AboutDialog::~AboutDialog() {
