628c0b908f422a829987f384a4b59f273e9f229c
Sergey Svistunov <zenden2k@gmail.com>
2025-02-23 18:39:24 +0300

    ShellExt: fix compilation error

--- Source/Core/CommonDefs.h
+++ Source/Core/CommonDefs.h
@@ -21,4 +21,8 @@
 #pragma warning(disable:4996)
 #pragma warning( disable: 4100 ) // unreferenced formal parameter
 
+constexpr auto IU_VIDEOFILES_EXTENSIONS = "asf;avi;mpeg;mpg;mp2;divx;vob;flv;wmv;mkv;mp4;ts;mov;mpeg2ts;3gp;mpeg1;mpeg2;mpeg4;mv4;rmvb;qt;hdmov;divx;m4v;ogv;m2v;webm";
+constexpr auto IU_AUDIOFILES_EXTENSIONS = "aa;aac;ac3;adx;ahx;aiff;ape;asf;asx;au;snd;aud;dmf;dts;dxd;flac;la;m4a;mmf;mod;mp1;mp2;mp3;mp4;mpc;ofr;oga;ogg;opus;pac;pxd;ra;rka;sb0;shn;tta;voc;vqf;wav;wma;wv;xm;cd;mqa;mid";
+constexpr auto IU_EXTENSIONS_LIST_SEPARATOR = ";";
+
 #endif
