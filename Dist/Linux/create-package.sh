c7e3c292f976da318096a19108250f740f601724
Sergey Svistunov <zenden2k@gmail.com>
2025-05-01 18:41:43 +0300

    Test 2

--- Dist/Linux/create-package.sh
+++ Dist/Linux/create-package.sh
@@ -36,18 +36,22 @@ then
 	echo "Detected Architecture : $Architecture"
 fi
 mkdir ../../Build/Linux/
-rm -rf ~/imgupload/
-mkdir -p ~/imgupload/usr/bin
-mkdir -p ~/imgupload/usr/share/imgupload/
-mkdir -p ~/imgupload/usr/share/imgupload/Scripts/
+
+temp_dir=$(mktemp -d)
+echo "Temp directory: ${temp_dir}"
+root_dir="${temp_dir}/root"
+mkdir -p "${root_dir}"
+mkdir -p "${root_dir}/usr/bin"
+mkdir -p "${root_dir}/usr/share/imgupload/"
+mkdir -p "${root_dir}/usr/share/imgupload/Scripts/"
 #rm ./imgupload/usr/share/imgupload/Scripts/*
 #mkdir -p ./imgupload/usr/share/imgupload/Update/
 #set -e
-#objcopy --strip-debug --strip-unneeded ../../Bld-Linux-${Architecture}/CLI/CLI ~/imgupload/usr/bin/imgupload
-$ObjCopy --strip-debug --strip-unneeded ../../Build/CLI/Release/CLI ~/imgupload/usr/bin/imgupload
-cp ../../Data/servers.xml ~/imgupload/usr/share/imgupload/servers.xml
-cp ../../Data/.env ~/imgupload/usr/share/imgupload/.env
-cp ../../Data/Scripts/*.nut ~/imgupload/usr/share/imgupload/Scripts/
+#objcopy --strip-debug --strip-unneeded ../../Bld-Linux-${Architecture}/CLI/CLI "${root_dir}/usr/bin/imgupload"
+$ObjCopy --strip-debug --strip-unneeded ../../Build/CLI/Release/CLI "${root_dir}/usr/bin/imgupload"
+cp ../../Data/servers.xml "${root_dir}/usr/share/imgupload/servers.xml"
+cp ../../Data/.env "${root_dir}/usr/share/imgupload/.env"
+cp ../../Data/Scripts/*.nut "${root_dir}/usr/share/imgupload/Scripts/"
 #cp ../../Data/Update/iu_serversinfo.xml ./imgupload/usr/share/imgupload/Update/iu_serversinfo.xml
 
 if [ ! -f ~/imgupload/usr/bin/imgupload ]; then
@@ -55,14 +59,17 @@ if [ ! -f ~/imgupload/usr/bin/imgupload ]; then
     exit
 fi
 
-chmod -R 0755 ~/imgupload/
-chmod -x ~/imgupload/usr/share/imgupload/Scripts/*.nut
-chmod -x ~/imgupload/usr/share/imgupload/servers.xml
+chmod -R 0755 "${root_dir}/"
+chmod -x "${root_dir}/usr/share/imgupload/Scripts/"*.nut
+chmod -x "${root_dir}/usr/share/imgupload/servers.xml"
 
-tar -cJf "../../Build/Linux/imgupload-${AppVersion}-${Architecture}.tar.xz" -C ~/imgupload *
+tar_archive="${temp_dir}/imgupload-${AppVersion}-${Architecture}.tar.xz"
+cd "${root_dir}" && tar -cJf "${tar_archive}" * && cd -
+cp "${tar_archive}" "../../Build/Linux/"
 
-mkdir -p ~/imgupload/DEBIAN
-cp control_ ~/imgupload/DEBIAN/control
-cp dirs ~/imgupload/DEBIAN/dirs
-sed -i -e "s/YOUR_ARCHITECTURE/$Architecture/g" -e "s/IU_APP_VER_CLEAN/${IU_APP_VER_CLEAN}/g" -e "s/IU_BUILD_NUMBER/${IU_BUILD_NUMBER}/g" ~/imgupload/DEBIAN/control
-fakeroot dpkg-deb --build ~/imgupload/ "../../Build/Linux/imgupload_${AppVersion}_${Architecture}.deb"
+mkdir -p "${root_dir}/DEBIAN"
+cp control_ "${root_dir}/DEBIAN/control"
+cp dirs "${root_dir}/DEBIAN/dirs"
+sed -i -e "s/YOUR_ARCHITECTURE/$Architecture/g" -e "s/IU_APP_VER_CLEAN/${IU_APP_VER_CLEAN}/g" -e "s/IU_BUILD_NUMBER/${IU_BUILD_NUMBER}/g" "${root_dir}/DEBIAN/control"
+fakeroot dpkg-deb --build "${root_dir}/" "../../Build/Linux/imgupload_${AppVersion}_${Architecture}.deb"
+rm -rf "${temp_dir}"
